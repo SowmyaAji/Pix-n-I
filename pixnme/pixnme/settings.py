8e71b38272bd8d665cc68da9a940c3ebab92cb9e
Sowmya <sowmya.aji@gmail.com>
2019-09-07 16:52:34 -0400

    Create .env file make changes to settings.py

--- pixnme/pixnme/settings.py
+++ pixnme/pixnme/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from dotenv load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'qnqqs5_amcwd847yu5=syjdik@ar*vp5)lkvw0j_owf31nl765'
+SECRET_KEY = os.getenv('PIXNME_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
